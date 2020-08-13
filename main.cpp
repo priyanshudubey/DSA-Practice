9967b0b6aae3bb10a0ca5a64770d152acec7e3ee
JerryTheNoob <priyanshudubey81@gmail.com>
2020-08-13 12:05:01 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int digitCount(int n){
+  int re = 0;
+  while(n>0){
+    n=n/10;
+    re++;
+  }
+  //cout<<re;
+  return re;
+}
+int main() {
+  int n;
+  cin>>n;
+  cout<<digitCount(n);
+  return 0;
+  //digitCount();
+}
